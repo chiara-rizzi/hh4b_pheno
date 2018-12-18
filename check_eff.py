abbb4edcec510f61e8131f8377b37f390e546eaa
chiara-rizzi <chiara_rizzi@hotmail.it>
2018-12-18 11:44:21 +0100

    add eff wrt previous selection

--- check_eff.py
+++ check_eff.py
@@ -7,6 +7,7 @@ import math
 
 parser=argparse.ArgumentParser()
 parser.add_argument("-f","--file",help="file name", type=str, default="ggHH_smallR-jets.lhco")
+parser.add_argument("-4bex","--exactly4b", help="select only events with exactly 4b (as opposed to >= 4b)", type=int, default=0)
 args=parser.parse_args()
 
 print("Ciao! :) ")
@@ -24,11 +25,29 @@ data = pd.read_csv(args.file, sep="\s+")
 # structude of the input file: 
 # #  typ      eta      phi      pt    jmas   ntrk   btag  had/em   dum1   dum2
 
-n_events=0
+n_events = {}
+labels = ['tot','4b', 'dR', 'pT', 'eta', 'Xhh', 'XWt']
+for l in labels:
+     n_events[l] = 0
+eff={}
+eff['atlas']={}
+eff['us']={}
+eff['atlas']['tot']=1
+eff['atlas']['4b']=0.49
+eff['atlas']['dR']=0.445
+eff['atlas']['pT']=0.42
+eff['atlas']['eta']=0.37
+eff['atlas']['Xhh']=0.19
+eff['atlas']['XWt']=0.175
+
+
+
+#n_events=0
 n_bjets=0 # count the number of b-tagged jets, to be set to zero at each event
-n_events_4b=0 # number of events with 4b
-n_events_dR=0 # number of events that pass dR selection
-n_events_pT=0 # number of events that pass pT selection
+#n_events_4b=0 # number of events with 4b
+#n_events_dR=0 # number of events that pass dR selection
+#n_events_pT=0 # number of events that pass pT selection
+#n_events_eta=0 # number of events that pass eta selection
 bjets_event = [] # list of lorentz vectors of the b-jets in the event
 
 def pair_jets(bjets):
@@ -101,15 +120,20 @@ def pass_pT(h1, h2):
                return True
      return False
 
+def pass_dEta(h1, h2):
+     if math.fabs(h1.eta - h2.eta) < 1.5:
+          return True
+     return False
+
 # loop on all the particles
 for index, row in data.iterrows():
     #if n_events>500: break
      if row['#']==0:
-          if n_events%5000==0:
-               print("Looking at event: "+str(n_events))
+          if n_events['tot']%5000==0:
+               print("Looking at event: "+str(n_events['tot']))
           # just finished previous event: check if it had >= 4 b-tagged jets, and compute quantities 
-          if n_bjets >=4:
-               n_events_4b+=1
+          if (not args.exactly4b and n_bjets >=4) or n_bjets==4:
+               n_events['4b']+=1
                i_h1, i_h2 = pair_jets(bjets_event) 
                h1 = bjets_event[i_h1[0]] + bjets_event[i_h1[1]]
                h2 = bjets_event[i_h2[0]] + bjets_event[i_h2[1]]
@@ -117,14 +141,16 @@ for index, row in data.iterrows():
                dR_h1 =  dR(bjets_event[i_h1[0]], bjets_event[i_h1[1]])
                dR_h2 =  dR(bjets_event[i_h2[0]], bjets_event[i_h2[1]])
                if pass_dR(dR_h1, dR_h2, m4j):
-                    n_events_dR +=1
+                    n_events['dR'] +=1
                     if pass_pT(h1, h2):
-                         n_events_pT +=1
+                         n_events['pT'] +=1
+                         if pass_dEta(h1,h2):
+                              n_events['eta'] +=1
           # it's a new event! set event-by-event counters to zero 
           n_bjets=0 
           bjets_event = []        
           # increase the number of events        
-          n_events+=1 
+          n_events['tot']+=1 
      if row["typ"]==4 and row["btag"]>0 and row["pt"]>40 and row["eta"]<2.5:
           n_bjets+=1
           n = set_lv(row["pt"], row["eta"], row["phi"], row["jmas"])
@@ -137,12 +163,24 @@ for index, row in data.iterrows():
 #     n_events_4b+=1
 
 # for the way the code is set up, I'm disregardin the last event        
-n_events = n_events-1.0
-print("N events: "+str(n_events))
-print("N events >=4 b-jets pt>40 GeV: "+str(n_events_4b))
-print("N events dR: "+str(n_events_dR))
-print("Acc x Eff 4b:", 1.0*n_events_4b/n_events * 1/(0.58*0.58))
-print("Acc x Eff dR:", 1.0*n_events_dR/n_events * 1/(0.58*0.58))
-print("Acc x Eff pT:", 1.0*n_events_pT/n_events * 1/(0.58*0.58))
+n_events['tot'] = n_events['tot']-1.0
 
 
+print('\nNumber of events')
+for l in labels:
+     print("N events",l,":", n_events[l])
+
+print('\nAcceptance x Efficiency')
+for l in labels:
+     if l=='tot': 
+          eff['us'][l]=1
+     else:
+          eff['us'][l]=1.0*n_events[l]/n_events['tot']* 1/(0.58*0.58)
+          print(l,"  atlas:",eff['atlas'][l],'   us:',eff['us'][l])
+
+print('\nRelative to previous selection')
+for i in range(1,len(labels)):
+     l = labels[i]
+     l_prev=labels[i-1]
+     if eff['us'][l_prev] >0:          
+          print(l,"  atlas:",eff['atlas'][l]/eff['atlas'][l_prev],'   us:',eff['us'][l]/eff['us'][l_prev])
